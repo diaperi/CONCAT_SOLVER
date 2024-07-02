e16ce6b37bacb70262314c0b66b0699c38822749
yoonseo <choiys613@gmail.com>
2024-07-02 11:09:19 +0900

    커밋 메세지

---
+++ FRONT/yoonseo/js/register.js
@@ -0,0 +1,11 @@
+// JavaScript 코드
+// document.getElementById('nextButton').addEventListener('click', function() {
+//     window.location.href = 'sign2.html'; // 이동할 HTML 파일 경로
+// });
+
+// JavaScript 코드
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('nextButton').addEventListener('click', function() {
+        window.location.href = 'sign2.html'; // 이동할 HTML 파일 경로
+    });
+});
