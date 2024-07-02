22b6a79bc36ccc881b0a1b148dbe553c9f9a5bdd
Hyen <checma0309@gmail.com>
2024-07-02 11:23:14 +0900

    test

---
+++ FRONT/hyeeun/js/mypage_change.js
@@ -0,0 +1,9 @@
+function cancelUpdate() {
+    // 모든 입력 필드를 초기화
+    document.getElementById('mypage_change_updateForm').reset();
+}
+
+function saveUpdate() {
+    // 데이터 저장 로직 구현
+    alert('회원정보가 저장되었습니다.');
+}
