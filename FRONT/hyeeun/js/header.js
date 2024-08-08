9f0970db3d76721e65b5cb6c26be125971603821
Hyen <checma0309@gmail.com>
2024-08-08 17:57:09 +0900

    test

---
+++ FRONT/hyeeun/js/header.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mypageDropdown = document.getElementById('mypage-dropdown');
+    
+    // 드롭다운 외부 클릭 시 닫기
+    window.addEventListener('click', (e) => {
+        const mypageBtn = document.getElementById('mypage-btn');
+        if (!mypageBtn.contains(e.target) && !mypageDropdown.contains(e.target)) {
+            mypageDropdown.style.display = 'none';
+        }
+    });
+});
+
