025200b0f990d38e36d6abe220fed7cfd3c3b1d6
235221 <yuna230508@gmail.com>
2024-08-13 13:58:10 +0900

    feat: 로그인, 이메일 연동

--- BACK/SolverWeb/src/main/resources/static/seoyun/js/myPageMain.js
+++ BACK/SolverWeb/src/main/resources/static/seoyun/js/myPageMain.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const downBarButton = document.querySelector('.myPageMain_downBar');
-    const targetSection = document.querySelector('.myPageMain_downBar');
     const upBarButton = document.querySelector('.myPageMain_UpBar');
-    const targetSection2 = document.querySelector('.myPageMain_container2');
+    const targetSection2 = document.querySelector('.header_navbar');
 
-    downBarButton.addEventListener('click', function() {
-        targetSection.scrollIntoView({ behavior: 'smooth' });
+    downBarButton.addEventListener('click', function () {
+        window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'});
     });
-    upBarButton.addEventListener('click', function() {
-        targetSection2.scrollIntoView({ behavior: 'smooth' });
+
+    upBarButton.addEventListener('click', function () {
+        targetSection2.scrollIntoView({behavior: 'smooth'});
     });
 });
