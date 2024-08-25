3dbc7e6ba6a5089dd7d689f6347797be211ba3c4
235221 <yuna230508@gmail.com>
2024-08-26 05:12:24 +0900

    feat: 휴지통

--- BACK/SolverWeb/src/main/resources/static/yuna/js/trashEmp.js
+++ BACK/SolverWeb/src/main/resources/static/yuna/js/trashEmp.js
@@ -2,18 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const emptyTrashBtn = document.getElementById('trash_EmptyTrashBtn');
     const modalPopup = document.querySelector('.trashModal_popup');
     const cancelBtn = document.getElementById('trashModal_cancel');
-    const deleteBtn = document.getElementById('trashModal_delete');
 
     emptyTrashBtn.addEventListener('click', function () {
         modalPopup.style.display = 'flex';
     });
 
     cancelBtn.addEventListener('click', function () {
-        window.location.href = 'trash.html';
-    });
-
-    deleteBtn.addEventListener('click', function () {
-        window.location.href = 'trashEmp.html';
+        closeModal();
     });
 
     window.onclick = function (event) {
