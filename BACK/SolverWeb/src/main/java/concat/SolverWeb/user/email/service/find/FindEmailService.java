cf6abad06ff18f80ccc8070361365072ca486fc4
235221 <yuna230508@gmail.com>
2024-08-28 02:58:42 +0900

    feat: 이메일 인증

--- BACK/SolverWeb/src/main/java/concat/SolverWeb/user/email/service/find/FindEmailService.java
+++ BACK/SolverWeb/src/main/java/concat/SolverWeb/user/email/service/find/FindEmailService.java
@@ -38,14 +38,14 @@ public class FindEmailService {
     }
 
     public String createEmailText(UserDTO user, String tempPassword) {
-        return "<div style='font-family: Arial, sans-serif; font-size: 16px; color: #333;'>"
+        return "<div style='font-family: Arial, sans-serif; font-size: 15px; color: #333;'>"
                 + "<h2 style='color: #4461F2;'>SOLVER 안내 메일</h2>"
                 + "<p>안녕하세요, " + user.getUserName() + "님!</p>"
                 + "<p>SOLVER에 요청하신 아이디와 임시 비밀번호를 보내드립니다.</p>"
                 + "<p>로그인 후 반드시 비밀번호를 변경해주세요.</p>"
                 + "<p>감사합니다.</p><br>"
-                + "<p style='font-size: 18px; font-weight: bold;'><b>아이디:</b> " + user.getUserId() + "</p>"
-                + "<p style='font-size: 18px; font-weight: bold;'><b>임시 비밀번호:</b> <span style='color: #4461F2;'>" + tempPassword + "</span></p>"
+                + "<p style='font-size: 16px; font-weight: bold;'><b>아이디:</b> " + user.getUserId() + "</p>"
+                + "<p style='font-size: 16px; font-weight: bold;'><b>임시 비밀번호:</b> <span style='color: #4461F2;'>" + tempPassword + "</span></p>"
                 + "<hr style='border: 0; border-top: 1px solid #eee;'>"
                 + "<p style='font-size: 12px; color: #999;'>이 메일은 SOLVER 시스템에 의해 자동으로 발송되었습니다.</p>"
                 + "</div>";
