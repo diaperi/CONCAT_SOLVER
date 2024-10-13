e32cbdbf1dca9126e52550ce2f8be75a265e89a5
diaperi0901@gmail.com <diaperi0901@gmail.com>
2024-10-13 14:52:53 +0900

    aws 설정

--- BACK/SolverWeb/src/main/java/concat/SolverWeb/myPage/trashCs/Config/S3Config.java
+++ BACK/SolverWeb/src/main/java/concat/SolverWeb/myPage/trashCs/Config/S3Config.java
@@ -9,34 +9,55 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import io.github.cdimascio.dotenv.Dotenv;
 
+//@Configuration
+//@EnableScheduling
+//public class S3Config {
+//
+//    @Value("${cloud.aws.credentials.accessKey}")
+//    private String accessKey;
+//
+//    @Value("${cloud.aws.credentials.secretKey}")
+//    private String secretKey;
+//
+//    @Value("${cloud.aws.region.static}")
+//    private String region;
+//
+//    @Value("${cloud.aws.s3.base.url}")
+//    private String bucketBaseUrl;
+//
+//    @Bean
+//    public S3Client s3Client() {
+//        return S3Client.builder()
+//                .region(Region.of(region))
+//                .credentialsProvider(StaticCredentialsProvider.create(
+//                        AwsBasicCredentials.create(accessKey, secretKey)))
+//                .build();
+//    }
+//
+//    @Bean
+//    public String bucketBaseUrl() {
+//        return bucketBaseUrl;
+//    }
+//}
 @Configuration
 @EnableScheduling
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    @Value("${cloud.aws.s3.base.url}")
-    private String bucketBaseUrl;
-
     @Bean
     public S3Client s3Client() {
+        Dotenv dotenv = Dotenv.load(); // .env 파일을 로드
+
+        String accessKey = dotenv.get("AWS_ACCESS_KEY_ID");
+        String secretKey = dotenv.get("AWS_SECRET_ACCESS_KEY");
+        String region = dotenv.get("AWS_REGION");
+
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .build();
-    }
-
-    @Bean
-    public String bucketBaseUrl() {
-        return bucketBaseUrl;
     }
 }
